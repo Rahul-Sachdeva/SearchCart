29d13e604e4f0a0d734672c733499bf237036799
Rahul-Sachdeva <rahulsachdeva0305@gmail.com>
2025-03-03 20:23:09 +0530

    Updated for deploying on vercel

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config()
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const mongoose = require('mongoose');
 const MongoClient = require('mongodb').MongoClient;
